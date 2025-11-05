475742c4b85335ef1340a66322085c7ab25c9940
ilya <ilifud98@mail.ru>
2025-11-05 23:36:34 +0700

    Финальные доработки

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from catalog.views import home, contacts
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("catalog/", views.catalog, name="catalog")
+    path("contacts/", views.contacts, name="contacts")
 ]
