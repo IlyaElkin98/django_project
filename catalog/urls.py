ad12e12e30d9ea2a5ec5726fb33de4031de4fa82
ilya <ilifud98@mail.ru>
2025-11-24 22:56:23 +0700

    Создан контроллер, реализована маршрутизация, создан пустой HTML файл one_product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from catalog.views import home, contacts
+from catalog.views import home, contacts, one_product
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts")
+    path("contacts/", views.contacts, name="contacts"),
+    path("one_product/<int:pk>", views.one_product, name="one_product")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 def home(request):
     return render(request, "home.html")
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def one_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product, }
+    return render(request, "one_product.html", context)
