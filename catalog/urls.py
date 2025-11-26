c05ed66ece2d645fe9f3fddcf91c220e68981eca
ilya <ilifud98@mail.ru>
2025-11-26 21:23:40 +0700

    в маршутизации добавлен app_name Созданы шаблоны главная и один продукт

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path("home/", views.home, name="home"),
--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("catalog/", include("catalog.urls"))
+    path("catalog/", include(("catalog.urls", "catalog"), namespace="catalog"))
 ]
 
 if settings.DEBUG:
